4d03f1419a376d6f26aa85d1867fceb869edc58b
Michael Pautov <michael.pautov@gmail.com>
2024-05-24 16:12:25 +0600

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
       <div className="w-full h-full bg-black">
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
         <script src="https://cdn.htmlgames.com/embed.js?game=PinballBreakout&amp;bgcolor=black"></script>
       </div>
   );
